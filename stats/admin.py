ce82106b043823f95ef0cc12082fc8d10939728c
espoir dev <espoirdiallo199@gmail.com>
2025-03-17 18:56:56 +0100

    Supprimer tous les fichiers pour préparer un nouvel historique

--- stats/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Performance
-
-@admin.register(Performance)
-class PerformanceAdmin(admin.ModelAdmin):
-    list_display = ('user', 'period', 'period_start', 'period_end', 'completion_rate', 'on_time_rate', 'bonus_amount')
-    list_filter = ('period', 'period_start', 'period_end')
-    search_fields = ('user__username', 'user__email')
